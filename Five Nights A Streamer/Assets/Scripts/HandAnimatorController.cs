774cfb655980341b91773091567a7117c7de61d5
Brodie Liverman <90534314+lilkahuna@users.noreply.github.com>
2023-10-31 14:16:14 -0400

    Added Hand Animations
    Finished the work for adding animations to hands by using the HandAnimationController.cs

--- Five Nights A Streamer/Assets/Scripts/HandAnimatorController.cs
+++ Five Nights A Streamer/Assets/Scripts/HandAnimatorController.cs
@@ -6,9 +6,21 @@ using UnityEngine.InputSystem;
 public class HandAnimatorController : MonoBehaviour
 {
     [SerializeField] private InputActionProperty triggerAction;
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private InputActionProperty gripAction;
+
+    private Animator anim;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        float triggerValue = triggerAction.action.ReadValue<float>();
+        float gripValue = gripAction.action.ReadValue<float>();
+
+        anim.SetFloat("Trigger", triggerValue);
+        anim.SetFloat("Grip", gripValue);
     }
 }
