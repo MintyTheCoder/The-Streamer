be9671c5331ed9e2eb602a9a61d5c14faaa25599
Brodie Liverman <90534314+lilkahuna@users.noreply.github.com>
2023-11-16 14:17:56 -0500

    Clock is finished

---
+++ Five Nights A Streamer/Assets/Scripts/ClockBehavior.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ClockBehavior : MonoBehaviour
+{
+    private Boolean isGameOver;
+    public TextMeshProUGUI clockTime;
+    public float delay = 120f;
+    public int time = 7;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = GameObject.Find("GameManager").GetComponent<GameManager>().isGameOver;
+        clockTime.text = time + ":00 PM";
+        StartCoroutine(IncrementClock());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator IncrementClock()
+    {
+        while (!isGameOver)
+        {
+            if (time >= 12)
+            {
+                Debug.Log("Game Over");
+                isGameOver = true;
+            }
+            else
+            {
+                yield return new WaitForSeconds(delay);
+                time++;
+                clockTime.text = time + ":00 PM";
+            }
+        }
+ 
+    }
+}
