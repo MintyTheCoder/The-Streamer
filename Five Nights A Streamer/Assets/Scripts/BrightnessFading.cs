c56a16777ea60e65cc0c74de52c3c28b5f654220
Hogz13 <tc.hart465@k12cpschools.com>
2023-12-01 13:01:17 -0500

    Finished UI Stuff

--- Five Nights A Streamer/Assets/Scripts/BrightnessFading.cs
+++ Five Nights A Streamer/Assets/Scripts/BrightnessFading.cs
@@ -8,11 +8,10 @@ public class BrightnessFading : MonoBehaviour
 {
     public Image image;
     public Slider brightnessSlider;
-
     // Start is called before the first frame update
     void Update()
     {
-        image = GetComponent<Image>();
+       image = GetComponent<Image>();
         var tempColor = image.color;
         tempColor.a = brightnessSlider.value;
         image.color = tempColor;
