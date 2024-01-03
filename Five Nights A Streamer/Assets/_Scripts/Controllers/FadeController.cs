14ba45ba20da6c04642b6d506fc7401af3709737
Brodie Liverman <90534314+lilkahuna@users.noreply.github.com>
2024-01-03 14:19:42 -0500

    Finished basic fading transitions.
    Adding the pop up of the game over menu and fading needs to be added.

--- Five Nights A Streamer/Assets/_Scripts/Controllers/FadeController.cs
+++ Five Nights A Streamer/Assets/_Scripts/Controllers/FadeController.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FadeController : MonoBehaviour
 {
-    public static bool StartSceneSwitch {get; set;}
+    public static bool StartSceneSwitch { private get; set; }
+    public static bool StartMenuSwitch { private get; set; }
     [SerializeField] Animator anim;
     [SerializeField] TextMeshProUGUI nightText;
     private int levelToLoad;
@@ -16,16 +18,21 @@ public class FadeController : MonoBehaviour
         StartSceneSwitch = false;
         nightText.text = SceneManager.GetActiveScene().name;
     }
-
+    
     void Update()
     {
         if (StartSceneSwitch == true)
         {
             FadeToNextLevel();
         }
+
+        if (StartMenuSwitch == true)
+        {
+            FadeToMenu();
+        }
     }
 
-    public void FadeToNextLevel()
+    private void FadeToNextLevel()
     {
         FadeToLevel(SceneManager.GetActiveScene().buildIndex + 1);
     }
@@ -36,8 +43,14 @@ public class FadeController : MonoBehaviour
         anim.SetTrigger("FadeOut");
     }
 
+    private void FadeToMenu()
+    {
+        FadeToLevel(0);
+    }
+
     public void OnFadeComplete()
     {
         SceneManager.LoadScene(levelToLoad);
     }
+
 }
